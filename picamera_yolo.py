404ec6393593b63177c5bac01c17f7a3a1da63b5
dojinkimm <dojinkim119@gmail.com>
2019-09-19 11:04:00 -0400

    picamera

---
+++ picamera_yolo.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+from imutils.video.pivideostream import PiVideoStream
+from imutils.video import FPS
+import argparse
+import imutils
+import time
+import cv2
+
+
+# created a *threaded *video stream, allow the camera sensor to warmup,
+# and start the FPS counter
+print("[INFO] sampling THREADED frames from `picamera` module...")
+vs = PiVideoStream().start()
+time.sleep(2.0)
+fps = FPS().start()
+
+# loop over some frames...this time using the threaded stream
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # update the FPS counter
+    fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
