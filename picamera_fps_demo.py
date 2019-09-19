c6199f1ca93d82b74a1c3b4df70411629f5eb026
Henry <dojinkim119@gmail.com>
2019-09-19 13:37:23 -0400

    code fix

--- picamera_fps_demo.py
+++ picamera_fps_demo.py
@@ -13,77 +13,25 @@ import imutils
 import time
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-n", "--num-frames", type=int, default=500,
-    help="# of frames to loop over for FPS test")
-ap.add_argument("-d", "--display", type=int, default=-1,
-    help="Whether or not frames should be displayed")
-args = vars(ap.parse_args())
-
-# initialize the camera and stream
-camera = PiCamera()
-camera.resolution = (320, 240)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(320, 240))
-stream = camera.capture_continuous(rawCapture, format="bgr",
-    use_video_port=True)
-
-# allow the camera to warmup and start the FPS counter
-print("[INFO] sampling frames from `picamera` module...")
-time.sleep(2.0)
-fps = FPS().start()
-
-# loop over some frames
-for (i, f) in enumerate(stream):
-    # grab the frame from the stream and resize it to have a maximum
-    # width of 400 pixels
-    frame = f.array
-    frame = imutils.resize(frame, width=400)
-
-    # check to see if the frame should be displayed to our screen
-    if args["display"] > 0:
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
-
-    # clear the stream in preparation for the next frame and update
-    # the FPS counter
-    rawCapture.truncate(0)
-    fps.update()
-
-    # check to see if the desired number of frames have been reached
-    if i == args["num_frames"]:
-        break
-
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-# do a bit of cleanup
-cv2.destroyAllWindows()
-stream.close()
-rawCapture.close()
-camera.close()
 
 # created a *threaded *video stream, allow the camera sensor to warmup,
 # and start the FPS counter
-print("[INFO] sampling THREADED frames from `picamera` module...")
+print("[INFO] THREADED frames from `picamera` module...")
 vs = PiVideoStream().start()
 time.sleep(2.0)
 fps = FPS().start()
 
 # loop over some frames...this time using the threaded stream
-while fps._numFrames < args["num_frames"]:
+while True:
     # grab the frame from the threaded video stream and resize it
     # to have a maximum width of 400 pixels
     frame = vs.read()
     frame = imutils.resize(frame, width=400)
 
     # check to see if the frame should be displayed to our screen
-    if args["display"] > 0:
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
     # update the FPS counter
     fps.update()
@@ -96,3 +44,60 @@ print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
+
+
+
+
+
+# construct the argument parse and parse the arguments
+#ap = argparse.ArgumentParser()
+#ap.add_argument("-n", "--num-frames", type=int, default=500,
+#    help="# of frames to loop over for FPS test")
+#ap.add_argument("-d", "--display", type=int, default=-1,
+#    help="Whether or not frames should be displayed")
+#args = vars(ap.parse_args())
+#
+## initialize the camera and stream
+#camera = PiCamera()
+#camera.resolution = (320, 240)
+#camera.framerate = 32
+#rawCapture = PiRGBArray(camera, size=(320, 240))
+#stream = camera.capture_continuous(rawCapture, format="bgr",
+#    use_video_port=True)
+#
+## allow the camera to warmup and start the FPS counter
+#print("[INFO] sampling frames from `picamera` module...")
+#time.sleep(2.0)
+#fps = FPS().start()
+#
+## loop over some frames
+#for (i, f) in enumerate(stream):
+#    # grab the frame from the stream and resize it to have a maximum
+#    # width of 400 pixels
+#    frame = f.array
+#    frame = imutils.resize(frame, width=400)
+#
+#    # check to see if the frame should be displayed to our screen
+#    if args["display"] > 0:
+#        cv2.imshow("Frame", frame)
+#        key = cv2.waitKey(1) & 0xFF
+#
+#    # clear the stream in preparation for the next frame and update
+#    # the FPS counter
+#    rawCapture.truncate(0)
+#    fps.update()
+#
+#    # check to see if the desired number of frames have been reached
+#    if i == args["num_frames"]:
+#        break
+#
+## stop the timer and display FPS information
+#fps.stop()
+#print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+#print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+#
+## do a bit of cleanup
+#cv2.destroyAllWindows()
+#stream.close()
+#rawCapture.close()
+#camera.close()
