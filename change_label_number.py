683912e047d0addc674226882e62f00c9b53a6e7
Henry <dojinkim119@gmail.com>
2019-11-10 13:57:17 -0500

    py file that helps change label number in txt

---
+++ change_label_number.py
@@ -0,0 +1,32 @@
+import os
+
+label_to_change = ['15']
+new_label = "0"
+
+
+# if labelled numbers are different, it changes to number I need
+# e.g. 15 0.xx 0.xx 0.xx 0.xx -> 0 0.xx 0.xx 0.xx 0.xx
+def main():
+    path = "PATH/TO/LABELS"
+    dir_list = os.listdir(path)
+
+    for txt in sorted(dir_list):
+        data = []
+        with open(path + txt) as file:
+            print(txt)
+
+            for line in file:
+                index = line[:2].rstrip()
+
+                # changes label number if in variable label_to_change
+                if index in label_to_change:
+                    data.append(new_label+line[2:])
+
+        if len(data) > 0:
+            with open(path+txt, "w") as file:
+                for d in data:
+                    file.write(d)
+
+
+if __name__ == '__main__':
+    main()
